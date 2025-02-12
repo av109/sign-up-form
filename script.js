830080720268fbe1bd41bbe8a7d15d101d22da63
ayushv <rwtsiya025@gmail.com>
2025-02-13 03:16:13 +0530

    updates logic

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let password = document.querySelector("#password").value;
-    let confirmPassword = document.querySelector("#confirmPassword").value;
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector("form");
     let error = document.querySelector("#error");
-    if(password == "" && confirmPassword == ""){
-        error.innerHTML = "*passwords do not match";
-    }
-    else if(password === confirmPassword){
-        error.innerHTML = "";
-    }
-})
+
+    form.addEventListener("submit", function(event) {
+        let password = document.querySelector("#password").value;
+        let confirmPassword = document.querySelector("#confirmPassword").value;
+
+        if (password === "" || confirmPassword === "") {
+            error.innerHTML = "*passwords cannot be empty";
+            event.preventDefault();
+        } else if (password !== confirmPassword) {
+            error.innerHTML = "*passwords do not match";
+            event.preventDefault();
+        } else {
+            error.innerHTML = "";
+        }
+    });
+});
